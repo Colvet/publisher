15a6afb086b0b11f11e0c1f780da905144bb3035
SHINHAK KIM <37060250+Colvet@users.noreply.github.com>
2020-06-03 19:48:13 +0900

    publisher

--- spring-cloud-stream-consumer/src/main/java/com/javatechie/spring/cloud/stream/api/SpringCloudStreamConsumerApplication.java
+++ spring-cloud-stream-consumer/src/main/java/com/javatechie/spring/cloud/stream/api/SpringCloudStreamConsumerApplication.java
@@ -1,5 +1,8 @@
 package com.javatechie.spring.cloud.stream.api;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,20 +11,63 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+
 @SpringBootApplication
-@EnableBinding(Sink.class)
+//@EnableBinding(Sink.class)
 public class SpringCloudStreamConsumerApplication {
 
-    private Logger logger = LoggerFactory.getLogger(SpringCloudStreamConsumerApplication.class);
+    Logger logger = LoggerFactory.getLogger(SpringCloudStreamConsumerApplication.class);
 
-    @StreamListener("input")
-    public void consumeMessage(Book book) {
-        logger.info("Consume payload : " + book);
-    }
+//    @StreamListener("input")
+//    public void consumeMessage(Book book) {
+//        logger.info("Consume payload : " + book);
+//        logger.info("book id :" + book.getId());
+//    }
 
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudStreamConsumerApplication.class, args);
+        Consume();
     }
+
+    private static Properties setConsumerProperty(){
+        Properties consumerProps = new Properties();
+        consumerProps.put("bootstrap.servers", "localhost:9092");
+        consumerProps.put("group.id", "file"); //
+        consumerProps.put("enable.auto.commit", "true");
+        consumerProps.put("auto.offset.reset", "latest");
+
+        // 메시지와 키와 값에 문자열을 사용하면 내장된 StringDeserializer를 지정하면 된다.
+        consumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        consumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        return consumerProps;
+    }
+
+    private static void Consume(){
+        Logger logger = LoggerFactory.getLogger(SpringCloudStreamConsumerApplication.class);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer(setConsumerProperty());
+        consumer.subscribe(Arrays.asList("file"));
+        try{
+            while(true) {
+
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records) {
+//                    logger.info("Topic: %s, Partition: %s, Offset: %d, Key: %s, Value: %s\n", record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                    logger.info("\n" + record.getClass());
+                }
+
+            }
+        }finally {
+            consumer.close();
+        }
+
+    }
+
 
 }
--- spring-cloud-stream-publisher/src/main/java/com/javatechie/cloud/stream/api/SpringCloudStreamPublisherApplication.java
+++ spring-cloud-stream-publisher/src/main/java/com/javatechie/cloud/stream/api/SpringCloudStreamPublisherApplication.java
@@ -1,5 +1,9 @@
 package com.javatechie.cloud.stream.api;
 
+import callBack.PeterCallback;
+import org.apache.kafka.clients.producer.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,21 +15,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Properties;
+import java.util.UUID;
+
 @SpringBootApplication
 @EnableBinding(Source.class)
 @RestController
 public class SpringCloudStreamPublisherApplication {
+
     @Autowired
     private MessageChannel output;
 
     @PostMapping("/publish")
     public Book publishEvent(@RequestBody Book book) {
         output.send(MessageBuilder.withPayload(book).build());
+        publish();
         return book;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudStreamPublisherApplication.class, args);
+        publish();
+    }
+
+    private static Properties setProducerProperty(){
+        Properties producerProps = new Properties();
+        producerProps.put("bootstrap.servers", "localhost:9092");
+        producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        producerProps.put("ack",1);
+        producerProps.put("retries", 1);
+        /*
+            배치사이즈가 작으면 대량전송시 유실됨 최소 1000이상 잡는게 효율적
+        */
+        producerProps.put("batch.size", 1);
+        producerProps.put("linger.ms", 1);
+        producerProps.put("buffer.memory", 24568545);
+        return producerProps;
+    }
+
+    private static void publish(){
+        Logger logger = LoggerFactory.getLogger(SpringCloudStreamPublisherApplication.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer(setProducerProperty());
+
+        logger.info("Pubulish Start");
+        for (int i=0; i<10; i++) {
+//            ProducerRecord<String, String> data = new ProducerRecord<String, String>;
+            producer.send(new ProducerRecord<String, String>("file", String.format("%d",i)));
+            logger.info(String.format("%d",i));
+
+
+        }
+        producer.close();
+        logger.info("Pubulish End");
     }
 
 }
